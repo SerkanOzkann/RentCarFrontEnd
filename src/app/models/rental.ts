96c85f3fd22fdf5bf87f52d1038029855222d61d
Serkan ÖZKAN <57104731+Delete1903@users.noreply.github.com>
2021-03-14 15:50:17 +0300

    14.03.2021

---
+++ src/app/models/rental.ts
@@ -0,0 +1,4 @@
+export interface Rental{
+    rentalId:number;
+    brandName:string;
+}
---
+++ src/app/services/brand.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Brand } from '../models/brand';
+import { BrandResponseModel } from '../models/brandResponseModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BrandService {
+  brands:Brand[] = [];
+  apiUrl="https://localhost:44352/api/brands/getall"
+  constructor(private httpClient:HttpClient) { }
+
+  getBrands():Observable<BrandResponseModel>{
+   return this.httpClient.get<BrandResponseModel>(this.apiUrl);
+  }
+}
